047e4f421d855cfe3d2e6300080d8e16025adc81
pashkovdv <pashkovdv@mail.ru>
2019-06-22 15:09:35 +0300

    пробы настройки webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello world!");
+import {config} from './modules/config';
+import AppService from './modules/app_service';
+import './modules/header.component';
+
+console.log('Config key:', config.key);
+
+const service = new AppService('Hello world');
+service.log();
+
+let x=11;
+let g = v => console.log(v);
+console.log(g(x))
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist',
+  },
+  devServer: {
+    overlay: true,
+  },
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+      },
+    ],
+  },
 }
